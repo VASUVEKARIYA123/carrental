f772b9d981be5b3b4cf221357307536d5978f5c5
Anish59312 <anish31442@gmail.com>
2024-03-26 21:59:53 +0530

    create package for controller

---
+++ src/main/java/com/example/carrental/Controller/userController.java
@@ -0,0 +1,10 @@
+package com.example.carrental.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class userController {
+	
+	@GetMapping("adduser")
+	public void addUser
+}
